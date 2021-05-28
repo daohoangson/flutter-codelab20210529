83b7984a103a16af9785905a717e67fe4396c88e
Dao Hoang Son <daohoangson@gmail.com>
2021-05-29 03:13:04 +0700

    Explain more about variables

--- dart_language_samples/002_variables.dart
+++ dart_language_samples/002_variables.dart
@@ -12,3 +12,26 @@ var image = {
   'tags': ['saturn'],
   'url': '//path/to/saturn.jpg'
 };
+
+void variables() {
+  var var1 = 'one';
+  print(var1);
+  var1 = 'oneone';
+  // var1 = 1; // invalid type
+
+  final var2 = 'two';
+  print(var2);
+  // var2 = 'twotwo'; // cannot be set more than once
+
+  const constant = 'FOREVER';
+  print(constant);
+  // constant = 'nope'; // cannot assign
+
+  String str1 = 'one';
+  print(str1);
+  // str1 = null; // str1 is non-null
+
+  String? str2 = 'two';
+  print(str2);
+  str2 = null;
+}
