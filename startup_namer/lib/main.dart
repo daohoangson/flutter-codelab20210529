aa78a91fbcbed0215481e0d7d9d33836de631ca7
Dao Hoang Son <daohoangson@gmail.com>
2021-05-29 02:11:09 +0700

    english_words v4

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,6 +8,8 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
@@ -14,7 +17,7 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text('Hello World'),
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
